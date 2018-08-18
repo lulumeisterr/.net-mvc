90c455ba0e76560f880e91cd49859bad6115bee0
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-17 22:47:56 -0300

    Exemplo de CRUD

---
+++ 05.Fiap.Web.MVC/05.Fiap.Web.MVC/Controllers/ComidaController.cs
@@ -0,0 +1,42 @@
+﻿using _05.Fiap.Web.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _05.Fiap.Web.MVC.Controllers
+{
+
+    // Simular um banco de dados
+    public class ComidaController : Controller
+
+    {
+
+        private static List<Comida> _banco = new List<Comida>();
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_banco); 
+        }
+
+        // GET: Comida
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Comida comida)
+        {
+            _banco.Add(comida);
+            TempData["msg"] = "Comida cadastrada";
+            return RedirectToAction("Listar");
+        }
+
+
+    }
+}
---
+++ 05.Fiap.Web.MVC/05.Fiap.Web.MVC/Models/Comida.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _05.Fiap.Web.MVC.Models
+{
+    public class Comida
+    {
+
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public bool Salgado { get; set; }
+        public string Descricao { get; set; }
+        public bool Vegetariano { get; set; }
+        public double Calorias { get; set; }
+
+
+
+
+    }
+}
